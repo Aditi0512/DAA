30024e5556a62c29e0281f4282bbbcc3f948e10f
Aditi0512 <101205106+Aditi0512@users.noreply.github.com>
2022-03-27 23:02:23 +0530

    Create linear.cpp

---
+++ Week1/linear.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+int t;
+cin>>t;
+while(t--)
+{
+int n;
+cin>>n;
+int arr[n];
+for(int i=0;i<n;i++)
+cin>>arr[i];
+int no;#
+cin>>no;
+int c=0,flag=0;
+for(int i=0;i<n;i++)
+{c++;
+if(no==arr[i])
+{flag=1;
+break;}
+}
+if(flag==1)
+cout<<"Present "<<c<<endl;
+else
+cout<<"Not present "<<c<<endl;
+}
+return 0;}
